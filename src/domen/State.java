9bec088e4085ea0a82e034c943ac53a44a1af1d0
urosrolovic <uros.rolovic@gmail.com>
2020-06-17 18:25:56 +0200

    state

---
+++ src/domen/State.java
@@ -0,0 +1,5 @@
+package domen;
+
+public interface State {
+
+}
---
+++ src/domen/StopirajStanje.java
@@ -0,0 +1,11 @@
+package domen;
+
+public class StopirajStanje implements State{
+
+	@Override
+	public void akcija(Context context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
